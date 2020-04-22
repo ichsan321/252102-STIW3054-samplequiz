32d8c18b066059cf79669d3d65abd81e4859222a
ichsan321 <55977240+ichsan321@users.noreply.github.com>
2020-04-22 21:33:18 +0800

    Add files via upload

---
+++ untitled/src/window1.java
@@ -0,0 +1,22 @@
+import javax.swing.*;
+
+public class window1 extends JFrame {
+
+    private JFrame f;
+
+    public window1(){
+        setTitle("Hello World");
+         new JFrame ("Hello World");
+        setSize(400,300);
+        setLayout(null);
+        setDefaultCloseOperation(f.EXIT_ON_CLOSE);
+        setVisible(true);
+
+}
+
+public static void main (String[] args) {
+        new window1();
+
+
+}
+}
